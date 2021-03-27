889bdcadc19345109810a16f0bcca46343558899
Tim <timson995@gmail.com>
2021-03-27 17:59:24 +0600

    js: aded Horizontal scrolling

---
+++ 4auto/webapp/static/js/hscroll.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const area = document.querySelector('.scroll_horizontal')
+
+  area.addEventListener('wheel', function (e) {
+    if (e.type != 'wheel') {
+      return;
+    }
+    let delta = ((e.deltaY || -e.wheelDelta || e.detail) >> 10) || 1;
+    delta = delta * (-1000);
+    area.scrollLeft -= delta;
+    e.preventDefault();
+
+  });
+
+
+});
