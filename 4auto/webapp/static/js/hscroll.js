7cc02705a216c155577b0ff0bdf8ad99b1560b90
Tim <timson995@gmail.com>
2021-03-30 17:03:28 +0600

    Templates: added sign in/up

--- 4auto/webapp/static/js/hscroll.js
+++ 4auto/webapp/static/js/hscroll.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
       return;
     }
     let delta = ((e.deltaY || -e.wheelDelta || e.detail) >> 10) || 1;
-    delta = delta * (-500);
+    delta = delta * (-250);
     area.scrollLeft -= delta;
     e.preventDefault();
 
